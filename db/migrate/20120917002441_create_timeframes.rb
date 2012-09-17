157cbf20d8cc4c1a684d4a2b13d6a30dd7653501
Michael Nelson <michael@ckye.net>
2012-09-16 19:33:24 -0700

    Final tweaks for timeframe

--- db/migrate/20120917002441_create_timeframes.rb
+++ db/migrate/20120917002441_create_timeframes.rb
@@ -1,9 +1,9 @@
 class CreateTimeframes < ActiveRecord::Migration
   def change
     create_table :timeframes do |t|
-      t.string :name
-      t.datetime :starts_at
-      t.datetime :ends_at
+      t.string :name, null: false, limit: 60
+      t.datetime :starts_at, null: false
+      t.datetime :ends_at, null: false
 
       t.timestamps
     end
