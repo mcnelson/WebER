782198c84b729c8eb9e330706fa034c9acbdbd01
Michael Nelson <michael@ckye.net>
2012-10-03 14:37:04 -0700

    Added web font mimetypes so that Sprokets correctly delivers fonts (suppresses warning)

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Weber
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Fonts for asset pipeline
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+#
+# tell Rack (and Sprockets) about modern font MIME types:
+Rack::Mime::MIME_TYPES['.ttf'] = 'application/x-font-ttf'
+Rack::Mime::MIME_TYPES['.eot'] = 'application/x-font-eot'
+Rack::Mime::MIME_TYPES['.otf'] = 'application/x-font-otf'
+Rack::Mime::MIME_TYPES['.woff'] = 'application/x-font-woff'
