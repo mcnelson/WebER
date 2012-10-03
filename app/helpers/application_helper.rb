bfcce0bb84461829d3757f2c49a8b9b05fa69652
Michael Nelson <michael@ckye.net>
2012-10-03 16:50:25 -0700

    Added full & page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def set_title(title)
+    content_for(:full_title) { "#{ title } - WebER" }
+    content_for(:page_title) { title }
+  end
+
   def weekdays_for_select
     Date::DAYNAMES.zip( Date::DAYNAMES.map { |d| d.downcase[0..2] } )
   end
