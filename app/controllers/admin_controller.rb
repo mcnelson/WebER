8548518216bd42266dfafb91fb68217a6c4031a3
Michael Nelson <michael@ckye.net>
2012-10-07 15:07:01 -0700

    Fix redirect loop

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,9 @@ class AdminController < ApplicationController
   before_filter :check_permissions
 
   def check_permissions
+    # Login if no user
+    redirect_to signin_path if current_user.nil?
+
     # Allow admin, workstudy to pass. This could be further restricted at the controller level
     return if current_user and ["admin", "workstudy"].include? current_user.permission_level
     # Kick out otherwise
