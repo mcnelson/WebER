69549712aadd1c7ea6cdc1fedf0c4d422884e9c5
Michael Nelson <michael@ckye.net>
2012-10-16 10:45:31 -0700

    Update specs to correct locations. Coming up next: make them pass

--- spec/controllers/admin/reservations_controller_spec.rb
+++ spec/controllers/admin/reservations_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe ReservationsController do
+describe Admin::ReservationsController do
 
   # This should return the minimal set of attributes required to create a valid
   # Reservation. As you add validations to Reservation, be sure to
--- spec/controllers/admin/semesters_controller_spec.rb
+++ spec/controllers/admin/semesters_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe SemestersController do
+describe Admin::SemestersController do
 
   # This should return the minimal set of attributes required to create a valid
   # Semester. As you add validations to Semester, be sure to
