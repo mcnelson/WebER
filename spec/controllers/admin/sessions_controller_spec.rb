6e4f63600c3160b11f07e0390a034001ce6a5a09
Michael Nelson <michael@ckye.net>
2012-10-16 10:45:31 -0700

    Update specs to correct locations. Coming up next: make them pass

---
+++ spec/controllers/admin/sessions_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe SessionsController do
+  pending "Write me please"
+# context 'with a valid user session' do
+#   before do
+#     @u = Factory(:user)
+#     session[:user_id] = @u.id
+#   end
+
+#   describe "POST create" do
+#     it "redirects" do
+#       post :create, { punet: @u.punet, password: @u.
+#       it {should_not redirect_to(signin_path)}
+#     end
+#   end
+# end
+
+# context 'with a nil user session' do
+#   before do
+#     u = Factory(:user)
+#     session[:user_id] = nil
+#   end
+
+#   it "redirects to the sign-in page" do
+#     .should redirect_to(signin_path)
+#   end
+# end
+end
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -18,7 +18,7 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe UsersController do
+describe Admin::UsersController do
 
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
