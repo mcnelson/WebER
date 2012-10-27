0da8d241fd3ee1f805161b4aa8ff0edc9d8a326f
Michael Nelson <michael@ckye.net>
2012-10-26 18:30:15 -0700

    Polish for user panels

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
+  def active_label(user)
+    if user.active
+      content_tag :span, "Active", class: "label label-success"
+    else
+      content_tag :span, "Inactive", class: "label label-inverse"
+    end
+  end
 end
