9eba12529fa53535207f675bf16aa28a2202cd70
Michael Nelson <michael@ckye.net>
2012-10-28 13:05:15 -0700

    Moved require_* permission functions to Application controller where they belong. Added blank student-level reservation controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,20 +2,4 @@ class AdminController < ApplicationController
   layout 'admin'
 
   before_filter :require_workstudy
-
-  def require_workstudy
-    # Login if no user
-    return redirect_to signin_path if current_user.nil?
-    return if current_user.workstudy?
-
-    # Kick out otherwise
-    render_403
-  end
-
-  def require_admin
-    return redirect_to signin_path if current_user.nil?
-    return if current_user.admin?
-
-    render_403
-  end
 end
