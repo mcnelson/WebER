d01e2c0121e215b83f23742665acf8a0a538d130
Michael Nelson <michael@ckye.net>
2012-10-28 20:27:07 -0700

    Implemented start/end date latest/earliest methods. Time relationships are almost there

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -12,6 +12,11 @@ class Reservation < ActiveRecord::Base
 
   accepts_nested_attributes_for :equipment
 
+  scope :find_by_range, lambda { |start_at, end_at|
+    where("starts_at > ?", start_at)
+      .where("ends_at < ?", end_at)
+  }
+
   STATUSES = [
     "live",
     "pending",
@@ -19,7 +24,51 @@ class Reservation < ActiveRecord::Base
     "missed"
   ]
 
+  def starts_at_day
+    starts_at.strftime('%a')[0..2].downcase
+  end
+
+  def ends_at_day
+    ends_at.strftime('%a')[0..2].downcase
+  end
+
+  def earliest_starts_at
+    er_hour = Semester
+      .for_date(starts_at).first
+      .er_hour.select { |erh| erh.day == starts_at_day }
+
+    !er_hour.empty? ? er_hour.first.starts_at : nil
+  end
+
+  def latest_starts_at
+    er_hour = Semester
+      .for_date(starts_at).first
+      .er_hour.select { |erh| erh.day == starts_at_day }
+
+    !er_hour.empty? ? er_hour.first.ends_at : nil
+  end
+
+  def earliest_ends_at
+    er_hour = Semester
+      .for_date(starts_at).first
+      .er_hour.select { |erh| erh.day == ends_at_day }
+
+    !er_hour.empty? ? er_hour.first.starts_at : nil
+  end
+
+  def latest_ends_at
+    er_hour = Semester
+      .for_date(starts_at).first
+      .er_hour.select { |erh| erh.day == ends_at_day }
+
+    !er_hour.empty? ? er_hour.first.ends_at : nil
+  end
+
   def self.formatted_statuses
     STATUSES.map { |s| s.capitalize }
+  end
+
+  def contains?(equipment)
+    equipment.exists? equipment
   end
 end
