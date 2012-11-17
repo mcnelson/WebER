485e2b30d062f3e5a0c1f576fc6a6b23a4908ff4
Michael Nelson <michael@ckye.net>
2012-11-17 15:25:48 -0800

    Validate correct type of category for an equipment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
   attr_accessible :title
   validates_presence_of :title
+
+  def grouped_select_options
+    [ "Test" => ::AccessoryCategory.all ]
+  end
+
+  def pretty_type_label
+    type.underscore.humanize.titlecase
+  end
 end
