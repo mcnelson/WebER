d59c6ee9614bbf70b666cafcca5a5fa90afdac85
Michael Nelson <michael@ckye.net>
2012-11-19 17:11:42 -0800

    Add reservation equipment/accessory nested fields, and split the model association. Validate various stuff. Reset reservation JS

--- app/models/equipment_reservation.rb
+++ app/models/equipment_reservation.rb
@@ -2,6 +2,8 @@ class EquipmentReservation < ActiveRecord::Base
   belongs_to :equipment
   belongs_to :reservation
 
+  validates_presence_of :equipment, :reservation
+
   # Indicates that this equipment was added to the reservation as
   # part of this package.
   belongs_to :package
