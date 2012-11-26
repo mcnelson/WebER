99b85486464e150cc21c29966abd4d1860b89ca1
Michael Nelson <michael@ckye.net>
2012-11-26 12:16:47 -0800

    Factories for all done models.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,62 @@
+include ActionDispatch::TestProcess # For fixture_file_upload
 
-Factory.define :user do |u|
-  u.punet 'asdf1234'
-  u.pu_student_id '0900991'
-  u.email 'test@testerson.com'
-  u.permission_level "student"
-  u.password 'asdf'
-  u.password_confirmation 'asdf'
+FactoryGirl.define do
+
+  factory :accessory_category do
+    title "HPX Batteries"
+  end
+
+  # Not sure why this says a duplicate -- TODO fix me
+ #factory :accessory_dependency do
+ #  equipment
+ #  accessory_category
+ #end
+
+  factory :equipment do
+    name      "HPX #1"
+    category
+    photo     fixture_file_upload('spec/images/hpx_test_photo.jpg')
+    brand     "Panasonic"
+    model     "HPX-4392"
+    serial    "MB0932310111"
+    # accessory - Up to the spec
+
+    active    true
+    notes     "Checked out with a little thing here and a little thing there"
+  end
+
+  factory :equipment_category do
+    title "Cameras"
+  end
+
+  factory :equipment_package do
+    # Write me
+  end
+
+  factory :er_hour do
+    day         Date.parse("Monday")
+    starts_at   Time.parse("1:00pm")
+    ends_at     Time.parse("3:00pm")
+  end
+
+  factory :reservation do
+    starts_at   Date.parse("Nov 12, 2012")
+    ends_at     Date.parse("Nov 14, 2012")
+
+    status
+  end
+
+  factory :user do
+    punet                 "asdf1234"
+    pu_student_id         "0900991"
+    email                 "test@testerson.com"
+    permission_level      "student"
+    password              "asdf"
+    password_confirmation "asdf"
+  end
+
+  factory :semester do
+    starts_at   Date.parse("Oct 24, 2012")
+    ends_at     Date.parse("Dec 14, 2012")
+  end
 end
