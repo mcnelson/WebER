206682fc55c0a37a699e96fed04066372e799539
Michael Nelson <michael@ckye.net>
2012-12-02 14:52:12 -0800

    Add accessories controller. Fix a lot of incorrect/outdated model properties. Use singularized accessory category title for accessories.

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,11 +5,4 @@ class Category < ActiveRecord::Base
   def humanize_type
     type.underscore.humanize.titlecase
   end
-
-  def self.grouped_select_options
-    {
-      ::EquipmentCategory.all => "Equipment Categories",
-      ::AccessoryCategory.all => "Accessory Categories"
-    }
-  end
 end
--- app/models/equipment_category.rb
+++ app/models/equipment_category.rb
@@ -1,3 +1,3 @@
 class EquipmentCategory < Category
-  has_many :equipment, dependent: :restrict
+  has_many :equipment, dependent: :restrict, foreign_key: :category_id
 end
