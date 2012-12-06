3205d5fd5d0bb78f65db75f8e703285050c63ac9
Michael Nelson <michael@ckye.net>
2012-12-05 21:14:31 -0800

    Remove next_available from ERHour and move to Semester#next_er_hour. Working on final in_reservations_in_range_exclusive

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -28,4 +28,28 @@ class Semester < ActiveRecord::Base
   def er_hour_around(date)
     er_hours.select { |erh| erh.day.wday == date.wday } || nil
   end
+
+  # Find an er hour given a date. If no er hour on the given date, goes forward
+  # to find the next hour. I.e. given a Friday, iterates to Monday and returns that
+  def next_er_hour(date)
+    idate = date
+    er_hour = nil
+    begin
+      idate = idate.tomorrow
+      er_hour = er_hour_around(idate.wday)
+
+    end while er_hour.nil? && idate < ends_at
+
+    er_hour
+  end
+
+  # Get the ER hour's start time given a Date
+  def to_er_hour_start(date)
+    er_hour_around(date).try(:starts_at)
+  end
+
+  # Get the ER hour's end time given a Date
+  def to_er_hour_end(date)
+    er_hour_around(date).try(:ends_at)
+  end
 end
