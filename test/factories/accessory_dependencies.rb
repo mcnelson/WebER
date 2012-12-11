f303029f45c2305b55e05a513bb712a48ee25719
Michael Nelson <michael@ckye.net>
2012-12-10 19:18:39 -0800

    Remove test folder that somehow showed up again

--- test/factories/accessory_dependencies.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :accessory_dependency do
-    equipment_id 1
-    accessory_equipment_id 1
-    optional false
-  end
-end
