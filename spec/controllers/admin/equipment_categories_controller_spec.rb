0976f60b4d17ccdf4aba69070a2599997d938d92
Michael Nelson <michael@ckye.net>
2012-12-10 20:57:44 -0800

    Move Sessions controller spec out of admin. Create the remainder of the incomplete controller specs pending

---
+++ spec/controllers/admin/equipment_categories_controller_spec.rb
@@ -0,0 +1,6 @@
+
+require 'spec_helper'
+
+describe Admin::EquipmentCategoriesController do
+  pending "Write me"
+end
---
+++ spec/controllers/admin/packages_controller_spec.rb
@@ -0,0 +1,6 @@
+
+require 'spec_helper'
+
+describe Admin::PackagesController do
+  pending "Write me"
+end
