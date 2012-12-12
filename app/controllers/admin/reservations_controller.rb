6d2fef4ff303989d396c65d3138cc18a9bcfb093
Michael Nelson <michael@ckye.net>
2012-12-12 09:51:09 -0800

    Remove dead equipment row partial. Working on front-end reservation. Cleaned up controller, spec.

--- app/controllers/admin/reservations_controller.rb
+++ app/controllers/admin/reservations_controller.rb
@@ -2,64 +2,37 @@ class Admin::ReservationsController < AdminController
 
   def index
     @reservations = Reservation.page params[:page]
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @reservations }
-    end
   end
 
   def show
     @reservation = Reservation.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @reservation }
-    end
   end
 
   def new
     @reservation = Reservation.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @reservation }
-    end
   end
 
   def edit
     @reservation = Reservation.find(params[:id])
-
-    if params[:add_equipment]
-      equipment_row
-    end
   end
 
   def create
     @reservation = Reservation.new(params[:reservation])
 
-    respond_to do |format|
-      if @reservation.save
-        format.html { redirect_to [:admin, @reservation], notice: 'Reservation was successfully created.' }
-        format.json { render json: @reservation, status: :created, location: @reservation }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
+    if @reservation.save
+      redirect_to [:admin, @reservation], notice: 'Reservation was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   def update
     @reservation = Reservation.find(params[:id])
 
-    respond_to do |format|
-      if @reservation.update_attributes(params[:reservation])
-        format.html { redirect_to [:admin, @reservation], notice: 'Reservation was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
+    if @reservation.update_attributes(params[:reservation])
+      redirect_to [:admin, @reservation], notice: 'Reservation was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
@@ -67,17 +40,6 @@ class Admin::ReservationsController < AdminController
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
 
-    respond_to do |format|
-      format.html { redirect_to admin_reservations_path }
-      format.json { head :no_content }
-    end
-  end
-
-  def equipment_row
-    @equipment = Equipment.find(params[:equipment_id])
-
-    respond_to do |format|
-      format.html { render partial: "equipment_row", locals: { idx: params[:idx], equipment: @equipment } }
-    end
+    redirect_to admin_reservations_path
   end
 end
--- spec/controllers/admin/reservations_controller_spec.rb
+++ spec/controllers/admin/reservations_controller_spec.rb
@@ -1,139 +1,15 @@
 require 'spec_helper'
 describe Admin::ReservationsController do
 
-  def valid_attributes
-    {}
-  end
-
   before do
     signin_as("admin")
   end
 
-  describe "GET index" do
-    it "assigns all reservations as @reservations" do
-      reservation = Reservation.create! valid_attributes
-      get :index, {}, valid_session
-      assigns(:reservations).should eq([reservation])
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested reservation as @reservation" do
-      reservation = Reservation.create! valid_attributes
-      get :show, {:id => reservation.to_param}, valid_session
-      assigns(:reservation).should eq(reservation)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new reservation as @reservation" do
-      get :new, {}, valid_session
-      assigns(:reservation).should be_a_new(Reservation)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested reservation as @reservation" do
-      reservation = Reservation.create! valid_attributes
-      get :edit, {:id => reservation.to_param}, valid_session
-      assigns(:reservation).should eq(reservation)
-    end
-  end
-
   describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Reservation" do
-        expect {
-          post :create, {:reservation => valid_attributes}, valid_session
-        }.to change(Reservation, :count).by(1)
-      end
-
-      it "assigns a newly created reservation as @reservation" do
-        post :create, {:reservation => valid_attributes}, valid_session
-        assigns(:reservation).should be_a(Reservation)
-        assigns(:reservation).should be_persisted
-      end
-
-      it "redirects to the created reservation" do
-        post :create, {:reservation => valid_attributes}, valid_session
-        response.should redirect_to(Reservation.last)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved reservation as @reservation" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Reservation.any_instance.stub(:save).and_return(false)
-        post :create, {:reservation => {}}, valid_session
-        assigns(:reservation).should be_a_new(Reservation)
-      end
-
-      it "re-renders the 'new' template" do
-        # Trigger the behavior that occurs when invalid params are submitted
-        Reservation.any_instance.stub(:save).and_return(false)
-        post :create, {:reservation => {}}, valid_session
-        response.should render_template("new")
-      end
+    it 'creates a new reservation' do
+      post :create, {}
+      @reservation = FactoryGirl.create(:reservation_with_five_equipment)
+      @reservation.shold be_persisted
     end
   end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested reservation" do
-        reservation = Reservation.create! valid_attributes
-        # Assuming there are no other reservations in the database, this
-        # specifies that the Reservation created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
-        Reservation.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, {:id => reservation.to_param, :reservation => {'these' => 'params'}}, valid_session
-      end
-
-      it "assigns the requested reservation as @reservation" do
-        reservation = Reservation.create! valid_attributes
-        put :update, {:id => reservation.to_param, :reservation => valid_attributes}, valid_session
-        assigns(:reservation).should eq(reservation)
-      end
-
-      it "redirects to the reservation" do
-        reservation = Reservation.create! valid_attributes
-        put :update, {:id => reservation.to_param, :reservation => valid_attributes}, valid_session
-        response.should redirect_to(reservation)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the reservation as @reservation" do
-        reservation = Reservation.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Reservation.any_instance.stub(:save).and_return(false)
-        put :update, {:id => reservation.to_param, :reservation => {}}, valid_session
-        assigns(:reservation).should eq(reservation)
-      end
-
-      it "re-renders the 'edit' template" do
-        reservation = Reservation.create! valid_attributes
-        # Trigger the behavior that occurs when invalid params are submitted
-        Reservation.any_instance.stub(:save).and_return(false)
-        put :update, {:id => reservation.to_param, :reservation => {}}, valid_session
-        response.should render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested reservation" do
-      reservation = Reservation.create! valid_attributes
-      expect {
-        delete :destroy, {:id => reservation.to_param}, valid_session
-      }.to change(Reservation, :count).by(-1)
-    end
-
-    it "redirects to the reservations list" do
-      reservation = Reservation.create! valid_attributes
-      delete :destroy, {:id => reservation.to_param}, valid_session
-      response.should redirect_to(reservations_url)
-    end
-  end
-
 end
