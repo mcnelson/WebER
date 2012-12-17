d644018f3af1df0379e4871a3d508ca563a006a2
Michael Nelson <michael@ckye.net>
2012-12-16 17:16:36 -0800

    Reservation date erroring, unavailable equipment support. Corrections for date logic, validations. Still problem with nested forms botching query for reserved_* association because :conditions doesn't realize units is an association

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -18,21 +18,24 @@ class AjaxController < ApplicationController
   end
 
   def check_unit_availability
-    return render nothing: true if params_missing_any? [:start_at, :end_at]
+    return raise "Missing date parameters" if params_missing_any? [:start_at, :end_at]
+    json = []
 
     start_at = params[:start_at].to_date
     end_at = params[:end_at].to_date
 
-    json = []
-
-    (params[:equipment].to_a + params[:accessories].to_a).each.map { |n| n.to_i } .each do |unit_id|
-      if (unit = Unit.find(unit_id))
-        json << {
-          id:         unit.id,
-          available:  unit.in_reservations_in_range_exclusive(start_at, end_at).empty?,
-          thumb:      unit.photo.url(:forty),
-          medium:     unit.photo.url(:twosixty)
-        }
+    if start_at > end_at
+      json = { date_error: "The start date is after the end date." }
+    else
+      (params[:equipment].to_a + params[:accessories].to_a).each.map { |n| n.to_i } .each do |unit_id|
+        if (unit = Unit.find(unit_id))
+          json << {
+            id:         unit.id,
+            available:  unit.in_reservations_in_range_exclusive(start_at, end_at).empty?,
+            thumb:      unit.photo.url(:forty),
+            medium:     unit.photo.url(:twosixty)
+          }
+        end
       end
     end
 
