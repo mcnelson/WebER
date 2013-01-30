7676739f7eed8919e7c53f0a3df02f56ed386c3c
Michael Nelson <michael@ckye.net>
2013-01-29 22:04:05 -0800

    Only enable dates on the datepicker that actually have ER hours (pretty fuckin' slick, eh?)

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -9,4 +9,16 @@ module ReservationsHelper
     label_class = (color) ? "label label-#{color}" : ""
     content_tag :span, status.capitalize, class: label_class
   end
+
+  # Outputs JSON data for all valid dates in the given date's semester
+  def valid_er_hour_days(date)
+    semester = Semester.around_date(date).first
+
+    days = []
+    semester.starts_at.upto(semester.ends_at) do |idate|
+      days << idate.strftime("%d/%m/%Y") unless semester.er_hour_on_day(idate).nil?
+    end
+
+    days.to_json
+  end
 end
