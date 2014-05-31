c7f5f2a1769dab70dc16dc60206d6f216570cda4
mcnelson <michael@nelsonware.com>
2014-05-31 09:14:55 -0700

    Refactor predicate method defining & test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has permission predicate methods' do
+    u = User.new(permission_level: 'student')
+    expect(u).to be_student
+
+    u = User.new(permission_level: 'workstudy')
+    expect(u).to be_workstudy
+
+    u = User.new(permission_level: 'admin')
+    expect(u).to be_admin
+  end
 end
