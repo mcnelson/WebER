eda3d5022e02503e4836b6bac100ef7c7e72af6c
mcnelson <michael@nelsonware.com>
2014-06-01 10:21:32 -0700

    Rename session support

--- spec/support/session_support.rb
+++ spec/support/session_support.rb
@@ -1,7 +1,6 @@
-module Sessions
+module SessionSupport
   def signin_as(permission_level)
     test_user = FactoryGirl.create(:user, permission_level: permission_level)
     session[:user_id] = test_user.id
   end
 end
-
