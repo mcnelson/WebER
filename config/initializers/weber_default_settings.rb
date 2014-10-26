22555b15fb1241c0fc3e51c515c69734d93f9759
mcnelson <michael@nelsonware.com>
2014-10-26 11:24:18 -0700

    Turn off reservation minimum lead time requirement

--- config/initializers/weber_default_settings.rb
+++ config/initializers/weber_default_settings.rb
@@ -1,7 +1,7 @@
 Weber::Application.configure do
   config.system_active = true
   config.default_max_reservation_period = 2.days
-  config.reservation_min_lead_time = 1.day
+  config.reservation_min_lead_time = 0
   config.reservation_max_lead_time = 2.weeks
   config.reservation_absolute_max_lead_time = 3.months
 end
