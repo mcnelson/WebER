eca303ad687da3463fb318b251bda085a064c05d
mcnelson <michael@nelsonware.com>
2014-11-01 11:05:24 -0700

    Use app-wide SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Weber::Application.configure do
-  config.force_ssl = false
+  config.force_ssl = true
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
